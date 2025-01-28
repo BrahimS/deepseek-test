a5a4f5f1803544f872b5c28234065384e267243d
brahims <bsourny@gmail.com>
2025-01-28 22:19:43 +0100

    feat(front): add ui:

--- src/app/pages/api/cgpt.ts
+++ src/app/pages/api/cgpt.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Configuration, OpenAIApi } from 'openai';
 
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
